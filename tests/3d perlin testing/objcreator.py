3c5d3a86083350d50c19e74f68eab6fa645025d0
Pawel Fiszer <16pfiszer@cheney.oxon.sch.uk>
2021-11-24 10:07:55 +0000

    Changed library from perlin_noise to perlin_numpy

--- tests/3d perlin testing/objcreator.py
+++ tests/3d perlin testing/objcreator.py
@@ -1,18 +1,13 @@
-
-from perlin_noise import PerlinNoise as Perlin
+from perlin_numpy import generate_perlin_noise_2d
 import numpy as np
 import time
 
 
 
-def generateChunk(chunkName,SIZE=300,seed=time.time()):
+def generateChunk(chunkName,SIZE=200,seed=time.time()):
     # Load perlin noise and set seed
-    noise = Perlin(octaves=1,seed=seed)
-    noise2 = Perlin(octaves=2,seed=seed)
-    noise3 = Perlin(octaves=4,seed=seed)
-    noise4 = Perlin(octaves=8,seed=seed)
-    noise5 = Perlin(octaves=16,seed=seed)
-
+    np.random.seed(int(seed))
+    noise = generateNoise(SIZE)
     verts = {}
     tris = []
     normals = {}
@@ -22,7 +17,7 @@ def generateChunk(chunkName,SIZE=300,seed=time.time()):
     Wfnormals = ''
     for x in range(0,SIZE):
         for y in range(0,SIZE):
-            verts[SIZE*x+y] = (x,y,generateNoise(x,y,SIZE,noise,noise2,noise3,noise4,noise5))
+            verts[SIZE*x+y] = (x,y,noise[x,y])
             Wavefront += f'v {float(verts[SIZE*x+y][0])} {float(verts[SIZE*x+y][1])} {float(verts[SIZE*x+y][2])}\n'
             if x > 0 and y > 0:
                 tris += [[SIZE*(x-1)+(y-1),SIZE*(x-1)+y,SIZE*x+y,'']]
@@ -50,9 +45,10 @@ def generateChunk(chunkName,SIZE=300,seed=time.time()):
     return True
 
 
-def generateNoise(x,y,SIZE,noise,noise2=None,noise3=None,noise4=None,noise5=None):
-    multiplier = 10
-    return noise([x/SIZE,y/SIZE])*multiplier + 1*noise2([x/SIZE,y/SIZE])*multiplier + 0.5*noise3([x/SIZE,y/SIZE])*multiplier + 0.125*noise4([x/SIZE,y/SIZE])*multiplier + 0.0625*noise5([x/SIZE,y/SIZE])*multiplier
+def generateNoise(SIZE):
+    multiplier = 50
+    scale = 5
+    return generate_perlin_noise_2d((SIZE,SIZE),(scale,scale))*multiplier + generate_perlin_noise_2d((SIZE,SIZE),(scale//2,scale//2))
 
 if __name__ == "__main__":
     generateChunk('test')
