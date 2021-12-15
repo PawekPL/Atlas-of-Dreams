866026e354c96239fa7d5a919b4110f7c0320fad
Pawel Fiszer <16pfiszer@cheney.oxon.sch.uk>
2021-12-15 13:08:10 +0000

    better tris & faces

--- tests/3d perlin testing/objcreator.py
+++ tests/3d perlin testing/objcreator.py
@@ -20,18 +20,17 @@ def generateChunk(chunkName,SIZE=100,seed=time.time()):
             verts[SIZE*x+y] = (x,noise[x,y],y)
             Wavefront += f'v {float(verts[SIZE*x+y][0])} {float(verts[SIZE*x+y][1])} {float(verts[SIZE*x+y][2])}\n'
             if x > 0 and y > 0:
-                tris += [[SIZE*(x-1)+(y-1),SIZE*(x-1)+y,SIZE*x+y,'']]
+                tris += [[SIZE*(x-1)+(y-1),SIZE*(x-1)+y,SIZE*x+y,len(tris)+1]]
             if x < SIZE-1 and y < SIZE-1:
-                tris += [[SIZE*x+y, SIZE*(x+1)+(y+1), SIZE*(x+1)+y,'']]
+                tris += [[SIZE*x+y, SIZE*(x+1)+(y+1), SIZE*(x+1)+y,len(tris)+1]]
 
     for i,tri in enumerate(tris):
         p1 = np.array(verts[tri[0]])
         p2 = np.array(verts[tri[1]])
         p3 = np.array(verts[tri[2]])
-        tris[i][3] = i
         normals[i] = np.cross(p2-p1, p3-p1)
         Wfnormals += f'vn {normals[i][0]} {normals[i][1]} {normals[i][2]}\n'
-        Wftris += f'f {tri[0]+1}//{tri[3]+1} {tri[1]+1}//{tri[3]+1} {tri[2]+1}//{tri[3]+1}\n'
+        Wftris += f'f {tri[0]+1}//{tri[3]} {tri[1]+1}//{tri[3]} {tri[2]+1}//{tri[3]}\n'
 
 
     Wfnormals += 'usemtl Material\ns off\n'
