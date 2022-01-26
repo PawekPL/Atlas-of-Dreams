79cfe20ee3c554d0bc57f8213733cb1f06f30fbf
Pawel Fiszer <16pfiszer@cheney.oxon.sch.uk>
2022-01-26 10:28:51 +0000

    get nearest element using scipy

--- tests/3d perlin testing/get_nearest_element.py
+++ tests/3d perlin testing/get_nearest_element.py
@@ -1,17 +1,16 @@
 import numpy as np
-def find_nearest(array, value):
-    array = np.asarray(array)
-    print(np.where(np.abs(array - value)==np.abs(array - value).min()))
-    idx = np.where(np.abs(array - value)==np.abs(array - value).min())
-    return array[idx]
+from scipy import spatial
 
-array = np.array([[60,1,-0.2,1,1],[60,1,1,1,1],[60,0,0,0,0],[90,-1,-1,-1,-1]])
+array = np.array([[0,1,-0.2,1,1,0],[0,1,1,1,1,0],[.1,0,0,0,0,0],[.3,-1,-1,-1,-1,0],[.1,-0.55,.4567,1,0,0],[.1,-0.55,.4567,1,0,.02],[-0.1,-0.55,.4567,1,0,.01]])
 print(array)
 # [ 0.21069679  0.61290182  0.63425412  0.84635244  0.91599191  0.00213826
 #   0.17104965  0.56874386  0.57319379  0.28719469]
 
-value = [60,1,1,1,1]
 
-print(array)
+value = np.random.random(6)
 
-print(find_nearest(array, value))
+tree = spatial.KDTree(array)
+_,id=tree.query(value)
+
+print(value)
+print(array[id])
