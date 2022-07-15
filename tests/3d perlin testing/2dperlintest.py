ef62c3c7fbd52b791f7eb100ed7e2e1fdd00e5f3
Pawek <44177078+PawekPL@users.noreply.github.com>
2022-07-15 19:34:07 +0100

    Extraction
    Extracted project files form subfolder

--- tests/3d perlin testing/2dperlintest.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from perlin_numpy import (
-    generate_perlin_noise_2d, generate_fractal_noise_2d
-)
-noise = generate_perlin_noise_2d((256, 256), (8, 8))
-plt.imshow(noise, cmap='gray', interpolation='lanczos')
-plt.colorbar()
-plt.show()
